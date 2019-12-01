a993a14e34c8ece1362c853c7848c4139f872459
koukyo1994 <arabiannight1994@yahoo.co.jp>
2019-12-01 18:05:20 +0100

    Initial Commit

---
+++ PencilKitMNIST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PencilKitMNIST
+//
+//  Created by 荒居秀尚 on 01.12.19.
+//  Copyright © 2019 荒居秀尚. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
